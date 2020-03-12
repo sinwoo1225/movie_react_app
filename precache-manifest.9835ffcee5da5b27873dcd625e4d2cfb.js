a2edf1e7dc62dcf621a51ce8ef019334e964a298
sinwoo1225 <48782832+sinwoo1225@users.noreply.github.com>
2020-03-12 21:25:19 +0900

    Updates

---
+++ precache-manifest.9835ffcee5da5b27873dcd625e4d2cfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d3a0f95b3552dcb8133c2db4d69940",
+    "url": "/movie_react_app/index.html"
+  },
+  {
+    "revision": "73ef6df2c2ce742e2f45",
+    "url": "/movie_react_app/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "e8b1ab80ce483233550e",
+    "url": "/movie_react_app/static/js/2.42df282d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_react_app/static/js/2.42df282d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ef6df2c2ce742e2f45",
+    "url": "/movie_react_app/static/js/main.bedcc55e.chunk.js"
+  },
+  {
+    "revision": "e28e66c9973435bdd1dd",
+    "url": "/movie_react_app/static/js/runtime-main.9593f801.js"
+  }
+]);
